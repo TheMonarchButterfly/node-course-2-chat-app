dfe6def7e87b0416f0d6ffd86b5390b258bcd808
TheMonarchButterfly <etqz@hotmail.com>
2017-05-15 18:09:08 +0800

    Emit newMessage on createMessage

--- server/server.js
+++ server/server.js
@@ -18,11 +18,11 @@ app.use(express.static(publicPath));
 io.on('connection', (socket) => {
   console.log('New user connected')
 
-  socket.emit('newMessage', {
-    from: 'John',
-    text: 'See you then',
-    createdAt: 123123
-  });
+  // socket.emit('newMessage', {
+  //   from: 'John',
+  //   text: 'See you then',
+  //   createdAt: 123123
+  // });
 
   // socket.emit('newEmail', {
   //   from: 'mike@example.com',
@@ -32,6 +32,11 @@ io.on('connection', (socket) => {
 
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
+    io.emit('newMessage', {
+      from: message.from,
+      text: message.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   // socket.on('createEmail', (newEmail) => {
