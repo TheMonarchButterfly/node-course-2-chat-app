e733e8f86c6cff3c0186f02c0f7c3f9f26094e5b
TheMonarchButterfly <etqz@hotmail.com>
2017-05-27 11:29:13 +0800

    Greet new user, and alert others

--- server/server.js
+++ server/server.js
@@ -30,12 +30,33 @@ io.on('connection', (socket) => {
   //   createAt: 123
   // });
 
+  // socket.emit from Admin text Welcome to the chat app
+  socket.emit('newMessage', {
+    from: 'Admin',
+    text: 'Welcome to the chat app'
+  });
+
+  // socket.broadcast.emit from Admin text New user joined
+  socket.broadcast.emit('newMessage', {
+    from: 'Admin',
+    text: 'New user joined',
+    createdAt: new Date().getTime()
+  });
+
   socket.on('createMessage', (message) => {
     console.log('createMessage', message);
     io.emit('newMessage', {
       from: message.from,
       text: message.text,
       createdAt: new Date().getTime()
+    });
+    // socket.broadcast.emit('newMessage', {
+    //   from: message.from,
+    //   text: message.text,
+    //   createdAt: new Date().getTime()
+    // });
+    socket.on('disconnect', () => {
+      console.log('User was disconnected');
     });
   });
 
